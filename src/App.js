ede475d70fb333b0cea66a146336fcef9ceb21b8
mjrfornari <mjrfornari@gmail.com>
2018-08-20 10:32:20 -0300

    123
    asdasdasd

--- src/App.js
+++ src/App.js
@@ -8,27 +8,27 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router>  
         <div className="App">
         <div className="App__Aside">
-          <div className="App__Aside__BG"></div>
+          {/* <div className="App__Aside__BG"></div> */}
+          <img src='/logo.png' className='App__Aside__BG' alt='Delphus'/>  
         </div>
         <div className="App__Form">
-          <div className="PageSwitcher">
-            <NavLink to="/entrar" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Entrar</NavLink>
-            <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Cadastrar</NavLink>
-          </div>
+            <div className="PageSwitcher">
+              <NavLink to="/entrar" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Entrar</NavLink>
+              <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Cadastrar</NavLink>
+            </div>
 
-          <div className="FormTitle"> 
-            <NavLink to="/entrar" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Entre</NavLink> ou <NavLink exact to="/" 
-            activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Cadastre-se</NavLink>
-          </div>
+            <div className="FormTitle"> 
+              <NavLink to="/entrar" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Entre</NavLink> ou <NavLink exact to="/" 
+              activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Cadastre-se</NavLink>
+            </div>
 
-          <Route exact path="/" component={CadastrarForm}>
-          </Route>
-          <Route path="/entrar" component={EntrarForm}>
-
-          </Route>
+            <Route exact path="/" component={CadastrarForm}>
+            </Route>
+            <Route path="/entrar" component={EntrarForm}>
+            </Route>
         </div>  
       </div>
       </Router>
