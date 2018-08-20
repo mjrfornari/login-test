c17ff1050f6b2300fbf55f874e88da772a6f6caf
mjrfornari <mjrfornari@gmail.com>
2018-08-20 08:40:57 -0300

    asd
    asd

--- src/pages/EntrarForm.js
+++ src/pages/EntrarForm.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 class EntrarForm extends Component {
     render(){
+      
         return(
           <div className="FormCenter">
           <form className="FormFields" onSubmit={this.handleSubmit}>
